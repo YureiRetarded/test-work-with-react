d3447e659d91d42ce4a2fa52c2dadda905d56a82
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-06-23 12:38:10 +0300

    first class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {useState} from "react";
 import Counter from "./Components/Counter";
+import ClassCounter from "./Components/ClassCounter";
 
 function App() {
 
     const [value, setValue] = useState('Текст')
-  return (
-    <div className="App">
-       <Counter/>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*//<Counter/>*/}
+            <ClassCounter/>
+        </div>
+    );
 }
 
 export default App;
