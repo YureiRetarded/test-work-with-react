565f6b23d1037ef3cf1fc5ab6c2aa13491e3d931
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-06-23 14:36:42 +0300

    Working with props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, {useState} from "react";
-import Counter from "./Components/Counter";
-import ClassCounter from "./Components/ClassCounter";
+import './Styles/App.css'
+import PostItem from "./Components/PostItem";
+import PostList from "./Components/PostList";
 
 function App() {
+    const [posts,setPosts]=useState([
+        {id:1, title:"text title", body:"more texts"},
+        {id:2, title:"text title", body:"more texts"},
+        {id:3, title:"text title", body:"more texts"},
+        {id:4, title:"text title", body:"more texts"},
+        {id:5, title:"text title", body:"more texts"},
+        {id:6, title:"text title", body:"more texts"},
+        {id:7, title:"text title", body:"more texts"}
+    ]);
 
-    const [value, setValue] = useState('Текст')
+    const [posts2,setPosts2]=useState([
+        {id:1, title:"text title111", body:"more texts"},
+        {id:2, title:"text title111", body:"more texts"},
+        {id:3, title:"text title111", body:"more texts"},
+        {id:4, title:"text title111", body:"more texts"},
+        {id:5, title:"text title111", body:"more texts"},
+        {id:6, title:"text title111", body:"more texts"},
+        {id:7, title:"text title111", body:"more texts"}
+    ]);
+
     return (
         <div className="App">
-            {/*//<Counter/>*/}
-            <ClassCounter/>
+        <PostList posts={posts} title={"List 1"}/>
+            <PostList posts={posts2} title={"List 2"}/>
         </div>
     );
 }
