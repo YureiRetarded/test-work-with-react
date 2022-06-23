36a7ff59ec83538bc0cb550a182e7654f67a359d
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-06-23 19:27:45 +0300

    Added delete post function

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
 import React, {useRef, useState} from "react";
 import './Styles/App.css'
-import PostItem from "./Components/PostItem";
 import PostList from "./Components/PostList";
-import MyButton from "./Components/UI/button/MyButton";
-import MyInput from "./Components/UI/input/MyInput";
+import PostForm from "./Components/PostForm";
 
 function App() {
-    const [posts,setPosts]=useState([
-        {id:1, title:"text title", body:"more texts"},
-        {id:2, title:"text title", body:"more texts"},
-        {id:3, title:"text title", body:"more texts"},
-        {id:4, title:"text title", body:"more texts"},
-        {id:5, title:"text title", body:"more texts"},
-        {id:6, title:"text title", body:"more texts"},
-        {id:7, title:"text title", body:"more texts"}
+    const [posts, setPosts] = useState([
+        {id: 1, title: "text title", body: "more texts"},
+        {id: 2, title: "text title", body: "more texts"},
+        {id: 3, title: "text title", body: "more texts"},
+        {id: 4, title: "text title", body: "more texts"},
+        {id: 5, title: "text title", body: "more texts"},
+        {id: 6, title: "text title", body: "more texts"},
+        {id: 7, title: "text title", body: "more texts"}
     ]);
-
-    const [posts2,setPosts2]=useState([
-        {id:1, title:"text title111", body:"more texts"},
-        {id:2, title:"text title111", body:"more texts"},
-        {id:3, title:"text title111", body:"more texts"},
-        {id:4, title:"text title111", body:"more texts"},
-        {id:5, title:"text title111", body:"more texts"},
-        {id:6, title:"text title111", body:"more texts"},
-        {id:7, title:"text title111", body:"more texts"}
-    ]);
-    const [title,setTitle]=useState("")
-    const [description,setDescription]=useState("")
-    const bodyInputRef = useRef()
-    const titleInputRef = useRef()
-    const addNewPost=(e)=>{
-        e.preventDefault()
-        //alert(title+"------"+description)
-        alert(titleInputRef.current.value+"------"+bodyInputRef.current.value)
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
     }
+
+    const removePost=(post)=>{
+        setPosts(posts.filter(p=>p.id!==post.id))
+    }
+
+    const [description, setDescription] = useState("")
+
 
     return (
         <div className="App">
-            <form>
-                {/*<MyInput value={title} ref={titleInputRef} onChange={event=>setTitle(event.target.value)} type="text" placeholder="title"/>*/}
-                {/*<MyInput value={description} ref={bodyInputRef} onChange={event=>setDescription(event.target.value)} type="text" placeholder="description"/>*/}
-                <MyInput  ref={titleInputRef}  type="text" placeholder="title"/>
-                <MyInput  ref={bodyInputRef}  type="text" placeholder="description"/>
-                <MyButton onClick={addNewPost} >Create post</MyButton>
-            </form>
-        <PostList posts={posts} title={"List 1"}/>
-            <PostList posts={posts2} title={"List 2"}/>
+            <PostForm create={createPost}/>
+            <PostList remove={removePost} posts={posts} title={"List 1"}/>
         </div>
     );
 }
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PostItem from "./PostItem";
 
-const PostList = ({posts,title}) => {
+const PostList = ({posts,title,remove}) => {
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {posts.map(post =>
-                <PostItem key={post.id} post={post}/>
+            {posts.map((post,index) =>
+                <PostItem remove={remove} number={index+1} key={post.id} post={post}/>
             )}
         </div>
     );
