57d3c99cf9880081507b94782a200a0cc483e38b
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-06-24 19:18:42 +0300

    Added filtering

--- src/App.js
+++ src/App.js
@@ -1,33 +1,73 @@
-import React, {useRef, useState} from "react";
+import React, {useMemo, useState} from "react";
 import './Styles/App.css'
 import PostList from "./Components/PostList";
 import PostForm from "./Components/PostForm";
+import MySelect from "./Components/UI/select/MySelect";
+import MyInput from "./Components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: "text title", body: "more texts"},
-        {id: 2, title: "text title", body: "more texts"},
-        {id: 3, title: "text title", body: "more texts"},
-        {id: 4, title: "text title", body: "more texts"},
-        {id: 5, title: "text title", body: "more texts"},
-        {id: 6, title: "text title", body: "more texts"},
-        {id: 7, title: "text title", body: "more texts"}
+        {id: 1, title: "aaa title", body: "cccc texts"},
+        {id: 2, title: "bbb title", body: "asda texts"},
+        {id: 3, title: "ccc title", body: "mosssre texts"},
+        {id: 4, title: "tdddext title", body: "awdawd texts"},
+        {id: 5, title: "rrrr title", body: "cccc texts"},
+        {id: 6, title: "teqqxt title", body: "qwert texts"},
+        {id: 7, title: "tertttxt title", body: "madvawgaore texts"}
     ]);
+    const  [selectedSort,setSelectedSort]=useState('');
+    const [searchQuery,setSearchQuery]=useState('')
+    const sortedPost=useMemo(()=>{
+        console.log('!!!!')
+        if(selectedSort)
+            return [...posts].sort((a,b)=>a[selectedSort].localeCompare(b[selectedSort]))
+        else
+            return posts
+    },[selectedSort,posts])
+
+    const sortedAndSearchedPosts = useMemo(()=>{
+        return sortedPost.filter(post=>post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+    },[searchQuery,sortedPost])
+
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
 
-    const removePost=(post)=>{
-        setPosts(posts.filter(p=>p.id!==post.id))
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
-
-    const [description, setDescription] = useState("")
-
+    const sortPost=(sort)=>{
+        setSelectedSort(sort);
+    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList remove={removePost} posts={posts} title={"List 1"}/>
+            <hr style={{marginTop:15,marginBottom:15}}/>
+            <div>
+                <MyInput
+                    placeholder='search'
+                    value={searchQuery}
+                    onChange={e=>setSearchQuery(e.target.value)}
+                />
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPost}
+                    defaultValue='Sort by:'
+                    option={[
+                        {value:'title',name:'Title'},
+                        {value:'body',name:'Description'}
+                    ]}
+                />
+            </div>
+            {sortedAndSearchedPosts.length !== 0
+                ?
+                <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"List 1"}/>
+                :
+                <h1 style={{textAlign: "center"}}>Posts doesn't exist</h1>
+            }
+
         </div>
     );
 }
