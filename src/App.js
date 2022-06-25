c7ebb0ef5f40382e29f9cf818a8ea7a20aa33cdb
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-06-25 16:21:10 +0300

    Added new hook

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ import {usePosts} from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./Components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+    const[fetchPosts,isPostsLoading,postError]=useFetching(async ()=>{
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    })
 
     useEffect(() => {
         fetchPosts();
@@ -30,13 +35,7 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    async function fetchPosts() {
-        setIsPostsLoading(true)
-        setTimeout(async ()=>{ const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false)},1000)
 
-    }
 
     return (
         <div className="App">
@@ -52,6 +51,7 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            {postError && <h1>Error:{postError}</h1>}
             {isPostsLoading
                 ? <div style={{display:'flex',justifyContent:'center',marginTop:50}}><Loader/></div>
                 : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"List 1"}/>
