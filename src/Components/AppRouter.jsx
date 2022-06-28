1674e1b60907648905723615396239b9d8ad678d
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-06-28 16:37:30 +0300

    navigation updated

--- src/Components/AppRouter.jsx
+++ src/Components/AppRouter.jsx
@@ -5,16 +5,14 @@ import Posts from "../pages/Posts";
 import Home from "../pages/Home";
 import Error from "../pages/Error";
 import PostIdPage from "../pages/PostIdPage";
+import {routes} from "./router";
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/posts" element={<Posts/>}/>
-            <Route path="/posts/:id" element={<PostIdPage/>}/>
-            <Route path="/" element={<Posts/>}/>
-            <Route path="/error" element={<Error/>}/>
-            <Route path='*' element={<Navigate to="/error"/>}/>
+            {routes.map(route=>
+                <Route path={route.path} element={route.element}/>
+            )}
         </Routes>
     );
 };
